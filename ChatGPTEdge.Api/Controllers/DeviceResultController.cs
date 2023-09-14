22038902362ab63b0cbc48470845e8e75909fa71
Yijing Lu <yijinglu@microsoft.com>
2023-09-13 22:39:55 -0700

    Added Manifest and Swagger

--- ChatGPTEdge.Api/Controllers/DeviceResultController.cs
+++ ChatGPTEdge.Api/Controllers/DeviceResultController.cs
@@ -1,5 +1,6 @@
 using ChatGPTEdge.Api.Managers;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace ChatGPTEdge.Api.Controllers
 {
@@ -21,18 +22,39 @@ namespace ChatGPTEdge.Api.Controllers
         /// Get the list of deviceResults
         /// </summary>
         [HttpGet(Name = "GetDenseCaptions")]
-        public IEnumerable<DeviceResult> Get(string imageUrl, string location)
+        public IEnumerable<DeviceResult> Get()
         {
             List<DeviceResult> response = new List<DeviceResult>();
-            DenseCaptionsResponse denseCaptionsResponse = this.denseCaptionManager.GetDenseCaptions(imageUrl);
-            response.Add(new DeviceResult() { Location = location, DenseCaptionsResponse = denseCaptionsResponse });
-            //using (StreamReader r = new StreamReader("ExampleResult.json"))
-            //{
-            //    string json = r.ReadToEnd();
-            //    DenseCaptionsResponse? result = JsonConvert.DeserializeObject<DenseCaptionsResponse>(json);
-            //    response.Add(new DeviceResult() { Location = "Farm Crop", DenseCaptionsResponse = result });
-            //}
+            //DenseCaptionsResponse denseCaptionsResponse = this.denseCaptionManager.GetDenseCaptions(imageUrl);
+            //response.Add(new DeviceResult() { Location = location, DenseCaptionsResponse = denseCaptionsResponse });
+            using (StreamReader r = new StreamReader("DenseCaptionsResult.json"))
+            {
+                string json = r.ReadToEnd();
+                DenseCaptionsResponse? result = JsonConvert.DeserializeObject<DenseCaptionsResponse>(json);
+                response.Add(new DeviceResult() { Location = "Farm Crop", DenseCaptionsResponse = result });
+            }
             return response;
+        }
+
+        [HttpGet("swagger.json")]
+        public IActionResult ServeOpenApiJson()
+        {
+            try
+            {
+                string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "swagger.json");
+                if (System.IO.File.Exists(filePath))
+                {
+                    return PhysicalFile(filePath, "application/json");
+                }
+                else
+                {
+                    return NotFound("File not found");
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"An error occurred: {ex.Message}");
+            }
         }
     }
 }
